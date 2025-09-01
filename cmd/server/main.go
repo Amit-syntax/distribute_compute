c7ebbc043a0e16ae9eb0cfe5e0165c0937700991
Amit-syntax <amitkr20020405@gmail.com>
2025-09-01 22:57:43 +0530

    new struct

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+
+func main() {
+	// TODO:
+	// run a websocket server
+	http.HandleFunc("/ws", )
+}
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Amit-syntax/distribute_compute
+
+go 1.24.6
