ff0039bd00ef6e8bb6d10fbfe26a30467a322b34
Amit-Syntax <amitkr20020405@gmail.com>
2025-09-27 16:54:25 +0000

    sending remote execution request to server

--- examples/dist_comp.py
+++ examples/dist_comp.py
@@ -13,12 +13,13 @@ import json
 
 host = ""
 port = ""
-_ws_conn = None
+_ws_conn: websocket.WebSocket
 _session_id = None
 
+
 def connect(
-    ws_host: str, ws_port: str, 
-    client_id: str, pip_modules: list,
+    ws_host: str, ws_port: str,
+    client_id: str, pip_modules: list[str],
 ):
 
     global host, port, _ws_conn, _session_id
@@ -32,6 +33,10 @@ def connect(
     }))
 
     response = _ws_conn.recv()
+    if type(response) is bytes:
+        response = response.decode("utf-8")
+
+    print("Response from server:", response)
     response = json.loads(response)
 
     _session_id = response["body"]["session_id"]
@@ -49,14 +54,20 @@ def get_clean_source(func):
             return unparse(node), node.name
     raise ValueError("No FunctionDef found in source")
 
+
 @dataclass
 class RemExecObj:
     func_source: str
     func_args: str
+    remote_call_uuid: str
     gpu_required: bool = True
 
+    def __repr__(self) -> str:
+        return f"RemExecObj({self.remote_call_uuid})"
+
+
 class RemoteExec:
-    
+
     def __init__(self, gpu_required: bool):
         self.gpu_required = gpu_required
         self.func = None
@@ -66,34 +77,49 @@ class RemoteExec:
         return self
 
     def remote(self, *args, **kwargs) -> RemExecObj:
+
+        args = str(args)
         source, func_name = get_clean_source(self.func)
-        source += f"\n{func_name}" + str(args)
+        source += f"\n{func_name}" + args
+
+        execution_uuid = self._remote_exec(
+            source, func_name, args
+        )
 
         return RemExecObj(
-            source, str(args),
+            source, args,
+            execution_uuid,
             self.gpu_required,
         )
 
-def get_results(rem_exec_objs: list[RemExecObj]):
-    # TODO: send code to server and get result
-    print("executing code...")
+    def _remote_exec(
+        self,
+        func_source: str, func_name: str,
+        func_args: str
+    ) -> str:
+        # TODO: send code to server and get result
+        # - catch exceptions from server if can't execute
+        rem_exec_call_obj = {
+            "type": "session_remote_exec_request",
+            "description": "Request to execute function remotely",
+            "body": {
+                "session_id": _session_id,
+                "func_source": func_source,
+                "func_args": func_args,
+                "func_name": func_name,
+                "params": {
+                    "gpu_required": self.gpu_required,
+                }
+            }
+        }
+        _ws_conn.send(json.dumps(rem_exec_call_obj))
 
-    results = []
+        resp = _ws_conn.recv()
+        if type(resp) is bytes:
+            resp = resp.decode("utf-8")
+        resp = json.loads(resp)
 
-    # TODO: here send functions to server and ask for execution
-    # - catch exceptions from server if can't execute
-    for rec in rem_exec_objs:
-        results.append(exec(rec.func_source))
-    
-    print(results)
+        print("Response from server:", resp)
+        execution_uuid = resp.get("body", {}).get("execution_id", None)
 
-
-
-@RemoteExec(gpu_required=True)
-def process_img(img_id: int):
-    print(f"processing img {img_id}")
-
-
-rem_exec_objs = [process_img.remote(i) for i in range(100)]
-
-get_results(rem_exec_objs)
+        return execution_uuid
